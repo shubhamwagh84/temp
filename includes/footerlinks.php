88fb01803485bf08163ea71b7a19b456226fd17c
Shubham Wagh <waghshubham84@gmail.com>
2021-03-27 23:30:32 +0530

    Add this file into includes folder

---
+++ includes/footerlinks.php
@@ -0,0 +1,48 @@
+</section>
+
+		<!-- Vendor -->
+		<script src="../assets/vendor/jquery/jquery.js"></script>
+		<script src="../assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+		<script src="../assets/vendor/bootstrap/js/bootstrap.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nanoscroller/0.8.7/javascripts/jquery.nanoscroller.min.js" integrity="sha512-Z1AU18k/qc1YyEkZkh+pTQ2I95w76DTli3cpFe03jaYTyX+Rsmvh5ZnTmi4Zg8gBc1QGbcwgpN0o1t9b9L1B2Q==" crossorigin="anonymous"></script>		<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/jquery.magnific-popup.min.js" integrity="sha512-+m6t3R87+6LdtYiCzRhC5+E0l4VQ9qIT1H9+t1wmHkMJvvUQNI5MKKb7b08WL4Kgp9K0IBgHDSLCRJk05cFUYg==" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/magnific-popup.min.css" integrity="sha512-nIm/JGUwrzblLex/meoxJSPdAKQOe2bLhnrZ81g5Jbh519z8GFJIWu87WAhBH+RAyGbM4+U3S2h+kL5JoV6/wA==" crossorigin="anonymous" />
+		<script src="../assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+		
+		<!-- Specific Page Vendor -->
+		<script src="../assets/vendor/jquery-ui/js/jquery-ui-1.10.4.custom.js"></script>
+		<script src="../assets/vendor/jquery-ui-touch-punch/jquery.ui.touch-punch.js"></script>
+		<script src="../assets/vendor/jquery-appear/jquery.appear.js"></script>
+		<script src="../assets/vendor/bootstrap-multiselect/bootstrap-multiselect.js"></script>
+		<script src="../assets/vendor/jquery-easypiechart/jquery.easypiechart.js"></script>
+		<script src="../assets/vendor/flot/jquery.flot.js"></script>
+		<script src="../assets/vendor/flot-tooltip/jquery.flot.tooltip.js"></script>
+		<script src="../assets/vendor/flot/jquery.flot.pie.js"></script>
+		<script src="../assets/vendor/flot/jquery.flot.categories.js"></script>
+		<script src="../assets/vendor/flot/jquery.flot.resize.js"></script>
+		<script src="../assets/vendor/jquery-sparkline/jquery.sparkline.js"></script>
+		<script src="../assets/vendor/raphael/raphael.js"></script>
+		<script src="../assets/vendor/morris/morris.js"></script>
+		<script src="../assets/vendor/gauge/gauge.js"></script>
+		<script src="../assets/vendor/snap-svg/snap.svg.js"></script>
+		<script src="../assets/vendor/liquid-meter/liquid.meter.js"></script>		
+		<!-- Theme Base, Components and Settings -->
+		<script src="../assets/javascripts/theme.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src="../assets/javascripts/theme.custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src="../assets/javascripts/theme.init.js"></script>
+
+		<!-- Datatable -->
+		<link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+		<script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+
+		<!-- jquery-validation -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js" integrity="sha512-UdIMMlVx0HEynClOIFSyOrPggomfhBKJE28LKl8yR3ghkgugPnG6iLfRfHwushZl1MOPSY6TsuBDGPK2X4zYKg==" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js" integrity="sha512-6Uv+497AWTmj/6V14BsQioPrm3kgwmK9HYIyWP+vClykX52b0zrDGP7lajZoIY1nNlX4oQuh7zsGjmF7D0VZYA==" crossorigin="anonymous"></script>
+
+		<!-- Webcam -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
+	</body>
+</html>
---
+++ includes/sidebar.php
@@ -0,0 +1,158 @@
+
+				<!-- start: sidebar -->
+				<aside id="sidebar-left" class="sidebar-left">
+				
+					<div class="sidebar-header">
+						<div class="sidebar-title">
+							Navigation
+						</div>
+						<div class="sidebar-toggle hidden-xs" data-toggle-class="sidebar-left-collapsed" data-target="html" data-fire-event="sidebar-left-toggle">
+							<i class="fa fa-bars" aria-label="Toggle sidebar"></i>
+						</div>
+					</div>
+				
+					<div class="nano">
+						<div class="nano-content">
+							<nav id="menu" class="nav-main" role="navigation">
+								<ul class="nav nav-main">
+									<li class="nav-active">
+										<a href="../dashboard.php">
+											<i class="fa fa-home" aria-hidden="true"></i>
+											<span>Dashboard</span>
+										</a>
+									</li>
+									<li class="nav-active">
+										<a href="department.php">
+											<i class="fa fa-sitemap" aria-hidden="true"></i>
+											<span>Department</span>
+										</a>
+									</li>
+									<li class="nav-parent">
+										<a>
+											<i class="fa fa-university" aria-hidden="true"></i>
+											<span>Academics</span>
+										</a>
+										<ul class="nav nav-children">
+											<li>
+												<a href="academic_year.php">
+													 Academic Year
+												</a>
+											</li>
+											<li>
+												<a href="semester.php">
+													 Semester 
+												</a>
+											</li>
+											<li>
+												<a href="division.php">
+													 Division 
+												</a>
+											</li>
+											<li>
+												<a href="batch.php">
+													 Batch 
+												</a>
+											</li>
+											<hr style="margin-top:1px; margin-bottom:1px; margin-left:10%; margin-right:10%; border-bottom: 1px solid #fff; width:80%;" />
+											<li>
+												<a href="ActiveDivision.php">
+													 Active Division 
+												</a>
+											</li>
+											<li>
+												<a href="ActiveBatch.php">
+													 Active Batch 
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-active">
+										<a href="faculty.php">
+											<i class="fa fa-users" aria-hidden="true"></i>
+											<span>Faculty</span>
+										</a>
+									</li>
+									<li class="nav-active">
+										<a href="course.php">
+											<i class="fa fa-book" aria-hidden="true"></i>
+											<span>Course</span>
+										</a>
+									</li>
+									<li class="nav-parent">
+										<a>
+											<i class="fa fa-list-alt" aria-hidden="true"></i>
+											<span>Time-Table</span>
+										</a>
+										<ul class="nav nav-children">
+											<li>
+												<a href="add-new.php">
+													 Master Timetable
+												</a>
+											</li>
+											<li>
+												<a href="theory_timetable.php">
+													 Theory Timetable
+												</a>
+											</li>
+											<li>
+												<a href="practical_timetable.php">
+													 Practical Timetable
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-parent">
+										<a>
+											<i class="fa fa-table" aria-hidden="true"></i>
+											<span>Conducted Sessions</span>
+										</a>
+										<ul class="nav nav-children">
+											<li>
+												<a href="theory_conducted.php">
+													 Theory
+												</a>
+											</li>
+											<li>
+												<a href="practical_conducted.php">
+													 Practical
+												</a>
+											</li>
+											<li>
+												<a href="details.php">
+													 Both
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-parent">
+										<a>
+											<i class="fa fa-calendar" aria-hidden="true"></i>
+											<span>Attendance</span>
+										</a>
+										<ul class="nav nav-children">
+											<li>
+												<a href="theory_attendance.php">
+													 Theory
+												</a>
+											</li>
+											<li>
+												<a href="practical_attendance.php">
+													 Practical
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-active">
+										<a href="student.php">
+											<i class="fa fa-graduation-cap" aria-hidden="true"></i>
+											<span>Students</span>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+				
+					</div>
+				
+				</aside>
+				<!-- end: sidebar -->
